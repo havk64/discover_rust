6e1a0b248a226488ebab92fb437d032650f7b08a
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-13 12:01:07 -0700

    Adding comments

--- first_cat.rs
+++ first_cat.rs
@@ -1,16 +1,21 @@
-use std::env;
-use std::io::BufReader;
-use std::fs::File;
-use std::io::prelude::*;
+///# Cat implementation with Rust.
+//
+//Modules loaded with brief description:
+use std::env;                   // To read command line arguments.
+use std::io::BufReader;         // To create a read buffer.
+use std::fs::File;              // To open the file.
+use std::io::prelude::*;        // To add some I/O traits.
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
- 
+// Getting the list of command line arguments.
+    let args: Vec<String> = env::args().
+
     if args.len() == 2 {
+        // Opening the file.
         let f = File::open(&args[1]).unwrap();
         let reader = BufReader::new(f);
-
+        // Printing each line.
         for line in reader.lines() {
             println!("{}", line.unwrap());
         }
